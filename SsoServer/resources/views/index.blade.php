bbc82677cfa3e93d4b36a6991c95c13789bdc871
mouyong <my24251325@gmail.com>
2023-03-10 16:10:41 +0800

    chore: 增加 sso server index 页

--- SsoServer/resources/views/index.blade.php
+++ SsoServer/resources/views/index.blade.php
@@ -9,5 +9,5 @@
     api_token: {{ \Plugins\SsoServer\Heplers\SsoCookieHelper::getServerCookieValue() ?? '未登录' }}
 
     <br>
-    <a href="https://authorization.hwecs.iwnweb.com/system-authorization" target="_blank">授权中心</a>
+    <a href="https://authorization.hwecs.iwnweb.com/system-authorization">授权中心</a>
 @endsection
