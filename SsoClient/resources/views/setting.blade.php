e64478c3fa143d83029095d9ae06f68d1a57b79c
mouyong <my24251325@gmail.com>
2023-03-10 13:33:44 +0800

    chore: 更新默认模板

--- SsoClient/resources/views/setting.blade.php
+++ SsoClient/resources/views/setting.blade.php
@@ -35,41 +35,4 @@
         </div>
     </div>
 </div>
-
-<script>
-    $(function () {
-        $(document).on('click', 'form button[type="submit"]', function (event) {
-            $(this).prepend('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> ');
-            $(this).prop('disabled', true);
-
-            $('form').submit();
-        });
-
-        $('form').submit(function(event) {
-            event.preventDefault();
-
-            $.ajax({
-                method: $(this).attr('method'),
-                url: $(this).attr('action'),
-                data: new FormData($(this)[0]),
-                contentType: false,
-                processData: false,
-                success: function(response) {
-                    console.log(response);
-
-                    $('.toast').toast('show');
-                    $('form button[type="submit"]').prop('disabled', false);
-                    window.location.reload();
-                },
-                error: function(error) {
-                    console.error(error);
-                    $('.toast').find('.toast-body').html(error.responseJSON.message || error.responseJSON.err_msg || '未知错误');
-                    $('.toast').toast('show');
-                    $('form button[type="submit"] span').remove();
-                    $('form button[type="submit"]').prop('disabled', false);
-                },
-            });
-        });
-    });
-</script>
 @endsection
