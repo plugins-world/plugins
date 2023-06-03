1634e15dadf7f511a7b2ca7ae4404970dd9c0e92
mouyong <my24251325@gmail.com>
2023-06-03 17:19:55 +0800

    feat: 更新 DcatSaas

--- bin/split.sh
+++ bin/split.sh
@@ -32,8 +32,10 @@ git pull origin $CURRENT_BRANCH || true
 # remote foundation git@github.com:mouyong/laravel-foundation.git
 # remote MarketManager git@github.com:mouyong/MarketManager.git
 remote LaravelSaas git@github.com:plugins-world/LaravelSaas.git
+remote DcatSaas git@github.com:plugins-world/DcatSaas.git
 
 
 # split 'src/Illuminate/Foundation' foundation
 # split 'MarketManager' MarketManager
 split 'LaravelSaas' LaravelSaas
+split 'DcatSaas' DcatSaas
