c89d2a462348581c65504546ac270a29d95ccf62
mouyong <my24251325@gmail.com>
2023-06-08 16:20:03 +0800

    chore: 更新模型User的性别常量名

--- LaravelJwtAuth/app/Models/User.php
+++ LaravelJwtAuth/app/Models/User.php
@@ -11,14 +11,14 @@ class User extends \App\Models\User
      * 
      * @see https://developers.weixin.qq.com/miniprogram/dev/api/open-api/user-info/UserInfo.html#number-gender
      */
-    const GENDER_UNKNOWN = 0;
-    const GENDER_MAN = 1;
-    const GENDER_FEMAN = 2;
-    const GENDER_MAP = [
-        User::GENDER_UNKNOWN => '未知',
-        User::GENDER_MAN => '男',
-        User::GENDER_FEMAN => '女',
-    ];
+     const GENDER_UNKNOWN = 0;
+     const GENDER_MALE = 1;
+     const GENDER_FEMALE = 2;
+     const GENDER_MAP = [
+         User::GENDER_UNKNOWN => '未知',
+         User::GENDER_MALE => '男',
+         User::GENDER_FEMALE => '女',
+     ];
 
     use SoftDeletes;
 
