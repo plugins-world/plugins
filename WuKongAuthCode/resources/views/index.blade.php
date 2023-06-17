2887c52bc664224786e27111fecd8f10432d1169
mouyong <my24251325@gmail.com>
2023-06-18 01:37:23 +0800

    chore: 更新分离脚本

--- WuKongAuthCode/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('WuKongAuthCode::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from plugin: {!! config('wu-kong-auth-code.name') !!}
-    </p>
-@endsection
